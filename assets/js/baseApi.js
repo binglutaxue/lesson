ac62922df6a470969e68f2422a9535c1c37aee8e
binglutaxue <330261243@qq.com>
2020-10-12 16:49:25 +0800

    主页制作完成

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,5 +1,24 @@
 const baseUrl = 'http://ajax.frontend.itheima.net';
 $.ajaxPrefilter(function (options) {
-    options.url = `${baseUrl}${options.url}`;   
+    options.url = `${baseUrl}${options.url}`;
     console.log(options);
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || '',
+        };
+    }
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    options.complete = function(res){       
+        if (
+            res.responseJSON.status === 1 &&
+            res.responseJSON.message === '身份认证失败！'
+        ) {
+            // 1. 强制清空 token
+            
+                localStorage.removeItem('token')
+                // 2. 强制跳转到登录页面
+                location.href = '/login.html'
+            
+        }
+    };
 })
