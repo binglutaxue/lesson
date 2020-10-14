b8c9bdab47bc131b9bcadc7127aa83f2617ba424
binglutaxue <330261243@qq.com>
2020-10-14 11:36:41 +0800

    个人信息模块完成

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,50 @@
+$(function() {
+    const form = layui.form
+    const layer = layui.layer;
+    form.verify({
+      nickname: function(value) {
+        if (value.length > 6) {
+          return '昵称长度必须在 1 ~ 6 个字符之间！'
+        }
+      }
+    })
+    initUserInfo()
+    function initUserInfo() {
+        $.ajax({
+          method: 'GET',
+          url: '/my/userinfo',
+          success: function(res) {
+            if (res.status !== 0) {
+              return layer.msg('获取用户信息失败！')
+            }
+            form.val('formUserInfo', res.data)
+          }
+        })
+    }
+    // 重置表单的数据
+    $('#btnReset').on('click', function(e) {
+    // 阻止表单的默认重置行为
+    e.preventDefault()
+    initUserInfo()
+    })
+
+    $('.layui-form').on('submit', function(e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault()
+        // 发起 ajax 数据请求
+        $.ajax({
+          method: 'POST',
+          url: '/my/userinfo',
+          data: $(this).serialize(),
+            success: function (res) {
+            //   console.log(res);
+            if (res.status !== 0) {
+              return layer.msg('更新用户信息失败！')
+            }
+              layer.msg('更新用户信息成功！')     
+            window.parent.getUserInfo()
+          }
+          
+        })
+      })
+  })
